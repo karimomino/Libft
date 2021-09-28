8cb0444a959f77437cbda8df6f1ced28ef804a62
Karim Amin <karimamin@Karims-MacBook-Air.local>
2021-09-28 18:34:19 +0400

    Added ft_strchr | no unit tests

---
+++ ft_strchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamin <kamin@42abudhabi.ae>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/28 18:16:00 by kamin             #+#    #+#             */
+/*   Updated: 2021/09/28 18:33:38 by kamin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char *strchr(const char *s, int c)
+{
+	int		len;
+	char	*string;
+
+	len = ft_strlen(s);
+	string = (char *)s;
+	while (len-- >= 0)
+	{
+		if (*string == c)
+			return (string);
+		string++;
+	}
+	return (NULL);
+}
