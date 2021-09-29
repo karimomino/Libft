61baa7a74d8a7dba7cefbaf9bfcf4c2cf1eae4b5
Karim Amin <kamin@lab2r1s12.42abudhabi.ae>
2021-09-29 18:45:18 +0400

    Added ft_calloc | no unit tests

---
+++ ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamin <kamin@42abudhabi.ae>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/29 16:47:10 by kamin             #+#    #+#             */
+/*   Updated: 2021/09/29 18:44:12 by kamin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = (void *)malloc(count * size);
+	ft_bzero(ptr, count);
+	return (ptr);
+}
