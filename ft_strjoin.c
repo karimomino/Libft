5d3559806ad7a61bb5a7961a2136f70158147978
Karim Amin <kamin@lab2r1s12.42abudhabi.ae>
2021-09-29 20:18:30 +0400

    Added ft_strjoin | no unit tests

---
+++ ft_strjoin.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamin <kamin@42abudhabi.ae>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/29 20:06:40 by kamin             #+#    #+#             */
+/*   Updated: 2021/09/29 20:17:52 by kamin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+	int		len1;
+	int		len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	joined = (char *)malloc(len1 + len2);
+	if (joined == NULL)
+		return (NULL);
+	else
+	{
+		ft_strlcat(joined, s1, len1);
+		ft_strlcat(joined, s2, len1 + len2);
+	}
+	return (joined);
+}
