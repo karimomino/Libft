0fe894ee2ed64bfc4e27d514d50a31ba550c595f
Karim Amin <karimamin@Karims-MacBook-Air.local>
2021-10-12 00:25:32 +0400

    Added ft_lstclear | no unit tests

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,25 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kamin <kamin@42abudhabi.ae>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/12 00:01:11 by kamin             #+#    #+#             */
-/*   Updated: 2021/10/12 00:24:07 by kamin            ###   ########.fr       */
+/*   Created: 2021/10/12 00:18:17 by kamin             #+#    #+#             */
+/*   Updated: 2021/10/12 00:25:25 by kamin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
-	if (!*lst)
-		*lst = new;
-	else
+	t_list *tmp;
+
+	if (*lst)
 	{
-		while ((*lst)->next)
-			(*lst) = (*lst)->next;
-		(*lst)->next = new;
+		while (*lst)
+		{
+			tmp = (*lst)->next;
+			del((*lst)->content);
+			free(*lst);
+			*lst = tmp;
+		}
+		*lst = NULL;
 	}
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kamin <kamin@42abudhabi.ae>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 10:01:22 by kamin             #+#    #+#             */
-/*   Updated: 2021/10/12 00:14:38 by kamin            ###   ########.fr       */
+/*   Updated: 2021/10/12 00:24:22 by kamin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,4 +64,5 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
 #endif
