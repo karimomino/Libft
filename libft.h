a7b2d90c32c2790a8a07da485fd565f85073fe6c
Karim Amin <karimamin@Karims-MacBook-Air.local>
2021-10-11 23:15:51 +0400

    added ft_putendl_fd | no unit tests

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kamin <kamin@42abudhabi.ae>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 10:01:22 by kamin             #+#    #+#             */
-/*   Updated: 2021/10/11 23:13:01 by kamin            ###   ########.fr       */
+/*   Updated: 2021/10/11 23:15:41 by kamin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,4 +50,5 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 #endif
