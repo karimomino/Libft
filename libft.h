bc47f082ef5bdeeec1cc77d64b1551266a0e28a1
Karim Amin <karimamin@Karims-MacBook-Air.local>
2021-10-11 23:55:59 +0400

    Added ft_lstadd_front | no unit tests

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kamin <kamin@42abudhabi.ae>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 10:01:22 by kamin             #+#    #+#             */
-/*   Updated: 2021/10/11 23:30:19 by kamin            ###   ########.fr       */
+/*   Updated: 2021/10/11 23:55:58 by kamin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,4 +59,5 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new)
 #endif
