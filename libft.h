946efb548a2a699bcad816b04ee730bc53c026ca
Karim Amin <karimamin@Karims-MacBook-Air.local>
2021-10-11 23:11:18 +0400

    added ft_putchar_fd | no unit tests

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kamin <kamin@42abudhabi.ae>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 10:01:22 by kamin             #+#    #+#             */
-/*   Updated: 2021/10/11 23:03:01 by kamin            ###   ########.fr       */
+/*   Updated: 2021/10/11 23:10:57 by kamin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,4 +48,5 @@ int		ft_atoi(const char *str);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
 #endif
