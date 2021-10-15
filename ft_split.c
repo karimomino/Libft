337eb2e7073ac0f71f40221ae155d1f12192fe1e
Karim Amin <kamin@lab1r1s8.42abudhabi.ae>
2021-10-15 04:03:34 +0400

    Split Fixed but not

--- ft_split.c
+++ ft_split.c
@@ -11,18 +11,14 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "ft_strtrim.c"
-#include "ft_strdup.c"
-#include "ft_strlen.c"
-#include "ft_strlcpy.c"
-#include "ft_substr.c"
-#include "ft_strchr.c"
 
 static int	ft_word_len(char *s, char c)
 {
 	int	word_len;
 
 	word_len = 0;
+	while(*s == c)
+		s++;
 	while (*s != c && *s++ != 0)
 		word_len++;
 	return (word_len);
@@ -33,18 +29,22 @@ static int	ft_word_count(char const *s, char c)
 	int word_count;
 
 	word_count = 0;
+	while(*s == c)
+		s++;
 	while (*s)
 	{
-		if (*s == c && *(s - 1) != c)
+		if (*s == c && *(s + 1) != c && *(s + 1) != '\0')
 			word_count++;
 		s++;
 	}
 	return (word_count + 1);
 }
 
-static char	**ft_alloc_split(char *sc, int wc)
+static char	**ft_alloc_split(char *sc, int wc, char c)
 {
 	char	**split;
+	while(*sc == c)
+		sc++;
 	if (ft_strlen(sc) == 0)
 		split = (char **)malloc(1 * sizeof(char *));
 	else
@@ -54,37 +54,52 @@ static char	**ft_alloc_split(char *sc, int wc)
 	return (split);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**ft_alloc_words(char **split, char *sc, char c, int wc)
 {
-	int		wc;
-	int		wl;
-	int		i;
-	char*	sc;
-	char**	split;
+	int	i;
+	int	wl;
 
-	sc = ft_strtrim(s, &c);
-	wc = ft_word_count(sc, c);
 	i = 0;
-	split = ft_alloc_split(sc, wc);
-	if (wc == 0 || *sc == '\0')
-		return (split);
 	while (i < wc)
 	{
-		wl = ft_word_len(sc, c);
-		split[i] = (char *)malloc((wl + 1) * sizeof(char));
-		ft_strlcpy(split[i], sc, wl + 1);
-		sc += wl;
-		while (*sc == c && c != 0)
+		while (*sc == c)
 			sc++;
+		if (*sc != '\0')
+		{
+			wl = ft_word_len(sc, c);
+			split[i] = (char *)malloc((wl + 1) * sizeof(char));
+			while (*sc == c && c != 0)
+				sc++;
+			ft_strlcpy(split[i], sc, wl + 1);
+			sc += wl;
+			while (*sc == c && c != 0)
+				sc++;
+		}
 		i++;
 	}
 	split[i] = 0;
 	return (split);
 }
 
+char	**ft_split(char const *s, char c)
+{
+	int		wc;
+	char*	sc;
+	char**	split;
+
+	sc = (char *)s;
+	wc = ft_word_count(sc, c);
+	split = ft_alloc_split(sc, wc, c);
+	if (wc == 0 || *sc == '\0')
+		return (split);
+	split = ft_alloc_words(split, sc, c, wc);
+	return (split);
+}
+
+// #include "ft_strlcpy.c"
+// #include "ft_strlen.c"
 // int main()
 // {
-// 	char * * tab = ft_split("  tripouille  42  ", ' ');
-// 	free(tab);
-// 	return 0;
+// 	char * * tab = ft_split("    ", ' ');
+// 	return (0);
 // }
